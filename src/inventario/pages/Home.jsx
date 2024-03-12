89dcddb4b008a43ade12066ce2af84034a6415c9
AlfredDev <alfredgarcia14200@gmail.com>
2024-03-12 10:27:28 -0600

    Creadno routes privadas y puiblicas para la AUUTH

--- src/inventario/pages/Home.jsx
+++ src/inventario/pages/Home.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../../auth/context/AuthContext'
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
+
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const onLoggout = () => {
+
+    logout();
+
+    navigate('/login', {
+      replace: true,
+    })
+  }
+
+
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <br />
+      <button onClick={onLoggout}>
+        Salir
+      </button>
+
+    </div>
   )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,13 +3,31 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { Home } from '../inventario/pages/Home'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { InventarioRoutes } from '../inventario/Routes/InventarioRoutes'
+import { PrivateRoute } from './PrivateRoute'
+import { PublicRouter } from './PublicRouter'
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path='login' element={<LoginPage />} />
-        <Route path='/*' element={<InventarioRoutes />} />
+
+        <Route path='login/*' element={
+          <PublicRouter>
+            <Routes>
+              <Route path='/*' element={<LoginPage />} />
+            </Routes>
+          </PublicRouter>
+        } />
+
+
+        <Route path='/*' element={
+          <PrivateRoute>
+            <InventarioRoutes />
+          </PrivateRoute>
+        } />
+
+        {/* <Route path='login' element={<LoginPage />} />
+        <Route path='/*' element={<InventarioRoutes />} /> */}
       </Routes>
     </>
   )
